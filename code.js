51fb0181e1e0410ff8735dc365fc2bf5f4e5a310
dev-alihasan <infomeorsendmehelp@protonmail.com>
2024-04-20 23:45:26 +0600

    variable chnage

--- code.js
+++ code.js
@@ -15,7 +15,7 @@ let nest = $("#nest"),
   egg_height = eggs.height(),
   egg_initial_position = parseInt(eggs.css("top")),
   score = 0,
-  life = 10,
+  life = 5,
   speed = 2,
   max_speed = 15,
   counter = 0,
@@ -127,18 +127,18 @@ restart.click(function () {
 });
 
 function collision($div1, $div2) {
-  var x1 = $div1.offset().left;
-  var y1 = $div1.offset().top;
-  var h1 = $div1.outerHeight(true);
-  var w1 = $div1.outerWidth(true);
-  var b1 = y1 + h1;
-  var r1 = x1 + w1;
-  var x2 = $div2.offset().left;
-  var y2 = $div2.offset().top;
-  var h2 = $div2.outerHeight(true);
-  var w2 = $div2.outerWidth(true);
-  var b2 = y2 + h2;
-  var r2 = x2 + w2;
+  let x1 = $div1.offset().left;
+  let y1 = $div1.offset().top;
+  let h1 = $div1.outerHeight(true);
+  let w1 = $div1.outerWidth(true);
+  let b1 = y1 + h1;
+  let r1 = x1 + w1;
+  let x2 = $div2.offset().left;
+  let y2 = $div2.offset().top;
+  let h2 = $div2.outerHeight(true);
+  let w2 = $div2.outerWidth(true);
+  let b2 = y2 + h2;
+  let r2 = x2 + w2;
 
   if (b1 < y2 || y1 > b2 || r1 < x2 || x1 > r2) return false;
   return true;
